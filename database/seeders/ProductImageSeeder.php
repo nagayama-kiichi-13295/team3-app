2a349a05cd22098f73e23fc1c0b24b96f10d150a
Masato Tamura <715-student012@la-study.com>
2026-06-22 11:07:36 +0900

    画像枚数アップ

--- database/seeders/ProductImageSeeder.php
+++ database/seeders/ProductImageSeeder.php
@@ -9,39 +9,19 @@ class ProductImageSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('product_images')->insert([
-            [
-                'product_id' => 1,
-                'image_path' => 'products/imagesproduct1_main.jpg',
+        $data = [];
+
+        for ($i = 1; $i <= 40; $i++) {
+            $data[] = [
+                'product_id' => $i,
+                'image_path' => "products/imagesproduct{$i}_main.jpg",
                 'is_main' => true,
                 'display_order' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
-            ],
-            [
-                'product_id' => 2,
-                'image_path' => 'products/imagesproduct2_main.jpg',
-                'is_main' => true,
-                'display_order' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'product_id' => 3,
-                'image_path' => 'products/imagesproduct3_main.jpg',
-                'is_main' => true,
-                'display_order' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'product_id' => 4,
-                'image_path' => 'products/imagesproduct4_main.jpg',
-                'is_main' => true,
-                'display_order' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
+            ];
+        }
+
+        DB::table('product_images')->insert($data);
     }
 }
